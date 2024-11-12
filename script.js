e6736c8805296b304db0e1d2698c184488da9d1e
Michelle Di Guglielmo <mfdigug@gmail.com>
2024-11-13 09:33:32 +1100

    added js alert to submit button

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
  */
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('woohoo!');
+});
+
